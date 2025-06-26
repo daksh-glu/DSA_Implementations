b267e653ee0433e1c83952fca5e3f3c355c76fc2
dakshjain2005 <147020461+dakshjain2005@users.noreply.github.com>
2025-06-26 09:46:08 +0530

    using recursion prnit name N times

---
+++ Print name N times.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void func(int p){
+   
+   while(p>0){
+       cout<<"DAKSH"<<endl;
+       p--;
+   }
+
+}
+
+int main(){
+  
+  .
+  int n = 4;
+  func(n);
+  return 0;
+}
